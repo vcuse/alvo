35d577ee296eb6b7fd87fc883097d4d585c774fb
Nico Ritschel <nico@elsl.de>
2021-02-27 10:23:25 -0800

    Blocks without a defined station are now ignored but don't make the program crash anymore

--- js/generator.js
+++ js/generator.js
@@ -7,9 +7,9 @@ goog.require('Blockly.JavaScript');
 var generated = [];
 
 Blockly.JavaScript['custom_start'] = function(block) {
-  if (generated[block]) 
+  if (generated[block.id]) 
     return '';
-  generated[block] = true;
+  generated[block.id] = true;
   var nextBlock = block.nextConnection && block.nextConnection.targetBlock();
   if (nextBlock) {
     return Blockly.JavaScript.blockToCode(nextBlock);
@@ -18,17 +18,29 @@ Blockly.JavaScript['custom_start'] = function(block) {
 };
 
 Blockly.JavaScript['custom_task'] = function(block) {
-  if (generated[block]) 
+  if (generated[block.id]) 
     return '';
-  generated[block] = true;
+  generated[block.id] = true;
   var site = block.getFieldValue('SITE');
-  var moveCode = 'Simulator[' + Simulator.instance + '].robot.moveToStation(Simulator[' + Simulator.instance + '].station["' + site + '"]';
   var nextBlock = block.nextConnection && block.nextConnection.targetBlock();
-  if (nextBlock) {
-    moveCode += ", function() {" + Blockly.JavaScript.blockToCode(nextBlock) + '})'
+
+  if (site != "NONE") {
+    var moveCode = 'Simulator[' + Simulator.instance + '].robot.moveToStation(Simulator[' + Simulator.instance + '].station["' + site + '"]';
+    if (nextBlock) {
+      moveCode += ", function() {" + Blockly.JavaScript.blockToCode(nextBlock) + '})'
+    }
+    else {
+      moveCode += ', function() { Simulator[' + Simulator.instance + '].idle = true; })';
+    }
   }
   else {
-    moveCode += ', function() { Simulator[' + Simulator.instance + '].idle = true; })';
+    var moveCode = '';
+    if (nextBlock) {
+      moveCode += Blockly.JavaScript.blockToCode(nextBlock);
+    }
+    else {
+      moveCode += 'Simulator[' + Simulator.instance + '].idle = true;';
+    }
   }
   return moveCode;
 };
