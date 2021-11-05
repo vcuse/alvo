8b7083ca630482442c5d3cd7efab21d76f4ce9b2
Nico Ritschel <ritschel@cs.ubc.ca>
2021-11-04 18:45:39 -0700

    Added new task + new tutorial

--- tut3/task.js
+++ tut3/task.js
@@ -1,25 +1,37 @@
 var initialized = false;
 
 var initTask = function() {
-  Simulator[Simulator.instance].station['STATIONA'] = new Station(document.getElementById("simulatordiv"), -80, -80, 'Station A');
-  Simulator[Simulator.instance].station['STATIONB'] = new Station(document.getElementById("simulatordiv"), 80, -80, 'Station B');
+  Simulator[Simulator.instance].station['STATIONA'] = new Station(document.getElementById("simulatordiv"), -100, -80, 'Station A');
+  Simulator[Simulator.instance].station['STATIONB'] = new Station(document.getElementById("simulatordiv"), 100, -80, 'Station B');
   Simulator[Simulator.instance].station['STATIONC'] = new Station(document.getElementById("simulatordiv"), 0, 100, 'Station C');
-  Simulator[Simulator.instance].station['STATIONA'].addItem("yellow", "left");
-  Simulator[Simulator.instance].station['STATIONB'].addItem("blue", "left");
-  Simulator[Simulator.instance].station['STATIONC'].addItem("green", "left");
-  Simulator[Simulator.instance].station['STATIONC'].addItem("orange", "left");
-  Simulator[Simulator.instance].station['STATIONC'].addItem("green", "right");
+  Simulator[Simulator.instance].station['STATIONA'].addItem("green", "left");
+  Simulator[Simulator.instance].station['STATIONA'].addItem("green", "left");
+  Simulator[Simulator.instance].station['STATIONA'].addItem("green", "left");
+  Simulator[Simulator.instance].station['STATIONB'].addItem("orange", "left");
+  Simulator[Simulator.instance].station['STATIONB'].addItem("orange", "left");
+  Simulator[Simulator.instance].station['STATIONB'].addItem("orange", "left");
+  Simulator[Simulator.instance].station['STATIONC'].addItem("yellow", "right");
+  Simulator[Simulator.instance].station['STATIONC'].addItem("yellow", "right");
+  Simulator[Simulator.instance].station['STATIONC'].addItem("yellow", "right");
 
   if (!initialized) {
     initialized = true;
     definedPositions["DEFAULT"] = [];
+    definedPositions["DEFAULT"]["top left"] = '"left",2';
+    definedPositions["DEFAULT"]["middle left"] = '"left",1';
     definedPositions["DEFAULT"]["bottom left"] = '"left",0';
+    definedPositions["DEFAULT"]["top right"] = '"right",2';
+    definedPositions["DEFAULT"]["middle right"] = '"right",1';
     definedPositions["DEFAULT"]["bottom right"] = '"right",0';
-    definedPositions["Move box to the right"] = [];
-    definedPositions["Move box to the right"]["bottom left"] = '"left",0';
-    definedPositions["Move box to the right"]["bottom right"] = '"right",0';
+    definedPositions["Move Stack"] = [];
+    definedPositions["Move Stack"]["top left"] = '"left",2';
+    definedPositions["Move Stack"]["middle left"] = '"left",1';
+    definedPositions["Move Stack"]["bottom left"] = '"left",0';
+    definedPositions["Move Stack"]["top right"] = '"right",2';
+    definedPositions["Move Stack"]["middle right"] = '"right",1';
+    definedPositions["Move Stack"]["bottom right"] = '"right",0';
     if (typeof toolboxRight != "undefined") {
-      var rightWorkspace = Blockly.inject('__Move box to the rightdiv',
+      var rightWorkspace = Blockly.inject('__Move Stackdiv',
       { media: pathPrefix + 'blockly/media/',
         toolbox: toolboxRight,
         trashcan: true,
@@ -30,33 +42,50 @@ var initTask = function() {
           wheel: false}
       });
       var workspaceBlocks = document.getElementById("rightWorkspaceBlocks");
-      rightWorkspaces['Move box to the right'] = rightWorkspace;
+      rightWorkspaces['Move Stack'] = rightWorkspace;
       rightWorkspace.registerToolboxCategoryCallback('LOCATIONS', flyoutLocationCategory);
       Blockly.Xml.domToWorkspace(workspaceBlocks, rightWorkspace);
       rightWorkspace.getBlocksByType("custom_taskheader")[0].getField("TASK").setValidator(taskValidator);
-      document.getElementById('__Move box to the rightdiv').style.display = 'none';
+      document.getElementById('__Move Stackdiv').style.display = 'none';
       rightWorkspace.addChangeListener(onTaskHeaderChanged);
       rightWorkspace.addChangeListener(logEvent);
     }
   }
 }
 
-var checkTask = function(instance) {
-  var expectYellow = Simulator[Simulator.instance].station['STATIONA'].rightItems[0];
-  var expectBlue = Simulator[Simulator.instance].station['STATIONB'].rightItems[0];
-  var expectGreen1 = Simulator[Simulator.instance].station['STATIONC'].leftItems[0];
-  var expectGreen2 = Simulator[Simulator.instance].station['STATIONC'].rightItems[0];
-  var expectOrange = Simulator[Simulator.instance].station['STATIONC'].rightItems[1];
+var checkTask = function() {
+  document.getElementById("surveylink").href = "https://ubc.ca1.qualtrics.com/jfe/form/SV_39ubStn0q6kxATY?UID=" + uid;
 
-  return expectYellow && expectBlue && expectOrange && expectGreen1 && expectGreen2 
-         && expectYellow.color == 'yellow' && expectBlue.color == 'blue' && expectGreen1.color == 'green' && expectGreen2.color == 'green' && expectOrange.color == 'orange' 
-         && (expectYellow.turned % 360) == 180 && (expectBlue.turned % 360) == 180 && (expectGreen1.turned % 360) == 0 && (expectGreen2.turned % 360) == 0 && (expectOrange.turned % 360) == 180;
+  var expectGreen1 = Simulator[Simulator.instance].station['STATIONA'].rightItems[0];
+  var expectGreen2 = Simulator[Simulator.instance].station['STATIONA'].rightItems[1];
+  var expectGreen3 = Simulator[Simulator.instance].station['STATIONA'].rightItems[2];
+  var expectOrange1 = Simulator[Simulator.instance].station['STATIONB'].rightItems[0];
+  var expectOrange2 = Simulator[Simulator.instance].station['STATIONB'].rightItems[1];
+  var expectOrange3 = Simulator[Simulator.instance].station['STATIONB'].rightItems[2];
+  var expectYellow1 = Simulator[Simulator.instance].station['STATIONC'].rightItems[0];
+  var expectYellow2 = Simulator[Simulator.instance].station['STATIONC'].rightItems[1];
+  var expectYellow3 = Simulator[Simulator.instance].station['STATIONC'].rightItems[2];
+  var greens = expectGreen1 && expectGreen2 && expectGreen3 
+               && expectGreen1.color == 'green' && (expectGreen1.turned % 360) == 0
+               && expectGreen2.color == 'green' && (expectGreen2.turned % 360) == 0
+               && expectGreen3.color == 'green' && (expectGreen3.turned % 360) == 0;
+  var oranges = expectOrange1 && expectOrange2 && expectOrange3 
+               && expectOrange1.color == 'orange' && (expectOrange1.turned % 360) == 0
+               && expectOrange2.color == 'orange' && (expectOrange2.turned % 360) == 0
+               && expectOrange3.color == 'orange' && (expectOrange3.turned % 360) == 0;
+  var yellows = expectYellow1 && expectYellow2 && expectYellow3 
+               && expectYellow1.color == 'yellow' && (expectYellow1.turned % 360) == 180
+               && expectYellow2.color == 'yellow' && (expectYellow2.turned % 360) == 180
+               && expectYellow3.color == 'yellow' && (expectYellow3.turned % 360) == 180;
+  return greens && oranges && blues && yellows;
 }
 
 setTimeout(function(){ submitLog("start", "0") }, 1000);
 
 var pathPrefix = "../";
-var taskId = "tut3";
+var taskId = "tut4";
+var startTime = Date.now();
+var maxTime = 1000 * 60 * 15;
 
 var taskStations = [[
       "Station A",
